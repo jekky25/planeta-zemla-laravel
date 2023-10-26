fdcd6ca028b64d3edfacb0fc185ed39f689c5357
jekky25 <jekky25@list.ru>
2023-10-26 21:01:44 +0300

    adding helpers file width google re-capcha keys

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+
+//google re-capcha keys
+define("RE_SITE_KEY","6LfD7VYkAAAAAC38VoG0yb2jeedEnV276kMTuwTb");
+define("RE_SEC_KEY","6LfD7VYkAAAAADvURBFXjV0Pjc1dxL_8NLt1RN47");
