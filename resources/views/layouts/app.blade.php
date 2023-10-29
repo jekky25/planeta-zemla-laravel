073b61a83a908d0b9cbd304fc7e346db04658f7e
jekky25 <jekky25@list.ru>
2023-10-29 22:18:59 +0300

    changing a doctype

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html>
+<!DOCTYPE html>
+<html lang="ru">
 <head>
 	<title>@yield('title')</title>
 	<base href="{{route('home')}}" />
