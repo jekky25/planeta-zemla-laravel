3ce2913684e54d76595538d5a2c99b7a59ce7dfe
jekky25 <jekky25@list.ru>
2023-11-01 13:08:57 +0300

    adding sapeServiceProvider

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         \App\Providers\MetaServiceProvider::class,
         \App\Providers\MenuServiceProvider::class,
+        \App\Providers\SapeServiceProvider::class, 
 
     ],
 
