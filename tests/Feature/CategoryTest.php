43e8bbb7f83d53ceae608ffb060dd8dd05d27703
jekky25 <jekky25@list.ru>
2024-05-08 08:09:49 +0300

    fix bug

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -11,7 +11,7 @@ class CategoryTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_country_page(): void
+    public function test_category_page(): void
     {
         $_SERVER['REQUEST_URI'] = '/zemlyakakplaneta/';
         $response = $this->get($_SERVER['REQUEST_URI']);
