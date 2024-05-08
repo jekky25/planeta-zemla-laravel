01f615d3264eb72e5dd104cb78ea49eb68a14bcb
jekky25 <jekky25@list.ru>
2024-05-08 06:33:26 +0300

    add phpUnit tests

---
+++ tests/Feature/CategoryTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CategoryTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_country_page(): void
+    {
+        $_SERVER['REQUEST_URI'] = '/zemlyakakplaneta/';
+        $response = $this->get($_SERVER['REQUEST_URI']);
+        $response->assertStatus(200);
+    }
+}
