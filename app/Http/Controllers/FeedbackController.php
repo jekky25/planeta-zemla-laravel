e9fd6c64d615d9c8e9d112cb52bafb30bf53a928
jekky25 <jekky25@list.ru>
2024-07-20 09:45:41 +0300

    make feedback request

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -5,10 +5,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Services\GoogleCaptchaService;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Contracts\Auth\Authenticatable;
+use App\Requests\FeedBackRequest;
 
 use Validator;
 use App\Helpers\Email;
@@ -22,7 +24,6 @@ class FeedbackController extends Controller
 	 */
 	public function __construct()
 	{
-		// $this->middleware('auth');
 	}
 
 	/**
@@ -43,61 +44,21 @@ class FeedbackController extends Controller
      * @param  \Illuminate\Http\Request  $request
 	 * @return \Illuminate\Http\Response
 	 */
-	public function sendFeedBack(Request $request)
+	public function sendFeedBack(FeedBackRequest $request)
 	{
-		$arParams = $request->post();
-		$rules = [
-			'name' 		=> ['required', 'string', 'max:30'],
-			'email' 	=> ['required', 'email'],
-			'subject'	=> ['required', 'max:300'],
-			'text'	 	=> ['required', 'max:1000']
-		];
-		$errMessages = ['name.required' 	=> 'Поле Имя не заполнено',
-						'name.max' 			=> 'Поле Имя должно быть не более :max символов',
-						'email.required' 	=> 'Поле Емайл не заполнено',
-						'email.email' 		=> 'Поле Емайл заполнено не корректно',
-						'subject.required' 	=> 'Тема не заполнена',
-						'subject.max'	 	=> 'Поле Тема должно быть не более :max символов',
-						'text.required' 	=> 'Комментарий не заполнен',
-						'text.max'	 	=> 'Ваш комментарий слишком длинный',
-		];
+		$arParams	= $request->post();
 
-		$validator = Validator::make($arParams, $rules, $errMessages);
+		$captcha	= new GoogleCaptchaService($arParams['recaptcha_response']);
+		$captcha->check();
 
-		if ($validator->fails()) {
-			return redirect('feedback')
-                        ->withErrors($validator)
-                        ->withInput();
-		}
-
-		$recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
-		$recaptcha_secret = RE_SEC_KEY;
-		$recaptcha_response = $arParams['recaptcha_response'];
-
-		$ch = curl_init();
-		curl_setopt_array($ch, [
-			CURLOPT_URL => $recaptcha_url,
-			CURLOPT_POST => true,
-			CURLOPT_POSTFIELDS => [
-			'secret' => $recaptcha_secret,
-			'response' => $recaptcha_response,
-			'remoteip' => $_SERVER['REMOTE_ADDR']
-		   ],
-		   CURLOPT_RETURNTRANSFER => true
-		  ]);
-	
-		$output = curl_exec($ch);
-		curl_close($ch);
-	
-		$recaptcha = json_decode($output);
-
-		if ($recaptcha->success === true && $recaptcha->score >= 0.5) {
-		} else {
+		if ($captcha->hasError())
+		{
 			$strError = 'Капча не пройдена';
 			return redirect('feedback')
 						->withErrors($strError)
 						->withInput();
 		}
+
 
 		$email_template = 'feedback';
 
---
+++ app/Requests/FeedBackRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Illuminate\Http\Request;
+
+class FeedBackRequest extends FormRequest
+{
+	/**
+	* rules for the request
+	* @return string array
+	*/
+	public function rules():array
+	{
+		return [
+			'name' 		=> ['required', 'string', 'max:30'],
+			'email' 	=> ['required', 'email'],
+			'subject'	=> ['required', 'max:300'],
+			'text'	 	=> ['required', 'max:1000']
+        ];
+	}
+
+	/**
+	* messages for the request
+	* @return string array
+	*/
+	public function messages():array
+	{
+		return	[
+					'name.required' 	=> 'Поле Имя не заполнено',
+					'name.max' 			=> 'Поле Имя должно быть не более :max символов',
+					'email.required' 	=> 'Поле Емайл не заполнено',
+					'email.email' 		=> 'Поле Емайл заполнено не корректно',
+					'subject.required' 	=> 'Тема не заполнена',
+					'subject.max'	 	=> 'Поле Тема должно быть не более :max символов',
+					'text.required' 	=> 'Комментарий не заполнен',
+					'text.max'	 	=> 'Ваш комментарий слишком длинный',
+				];
+	}
+}
