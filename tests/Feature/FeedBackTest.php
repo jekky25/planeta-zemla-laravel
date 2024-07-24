1c50262bdba3407643def9e27e1a125785247af2
jekky25 <jekky25@list.ru>
2024-07-24 07:40:53 +0300

    remove spaces

--- tests/Feature/FeedBackTest.php
+++ tests/Feature/FeedBackTest.php
@@ -8,13 +8,13 @@ use Tests\TestCase;
 
 class FeedBackTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_feed_back_page(): void
-    {
-        $_SERVER['REQUEST_URI'] = '/feedback/';
-        $response = $this->get($_SERVER['REQUEST_URI']);
-        $response->assertStatus(200);
-    }
+	/**
+	* A basic feature test example.
+	*/
+	public function test_feed_back_page(): void
+	{
+		$_SERVER['REQUEST_URI'] = '/feedback/';
+		$response = $this->get($_SERVER['REQUEST_URI']);
+		$response->assertStatus(200);
+	}
 }
