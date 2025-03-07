11db0bce6288dabe09dc2ba94d4d6418826e937c
jekky25 <jekky25@list.ru>
2025-03-07 10:34:47 +0300

    refactoring

--- resources/js/Pages/Post.vue
+++ resources/js/Pages/Post.vue
@@ -3,231 +3,24 @@
 		<div class="componentheading"></div>
 		<h2 v-if="post.category" class="contentheading"><Link :href="route('item.name',[post.category.slug, post.id, post.slug])" class="contentpagetitle">{{ post.title }}</Link></h2>
 		<div class="article-content"><span v-html="post.introtext"></span><span v-html="post.fulltext"></span></div>
-		<div v-if="post.comments" id="jc">
-			<div id="comments">
-				<h4>Комментарии
-					<a class="rss" :href="route('comment.rss',[post.category.slug, post.id, post.slug])" title="RSS лента комментариев этой записи" target="_blank">&nbsp;</a>
-				</h4>
-				<div id="comments-list" class="comments-list">
-					<template v-for="(comment, index) in post.comments">
-						<div class="even" :id="`comment-item-${comment.id}`">
-							<div class="rbox">
-								<div class="rbox_tr">
-									<div class="rbox_tl">
-										<div class="rbox_t">&nbsp;</div>
-									</div>
-								</div>
-								<div class="rbox_m">
-									<div class="comment-box usertype-guest">
-										<span class="comments-vote">
-											<span :id="`comment-vote-holder-${comment.id}`">
-												<a v-if="comment.votes" href="#" class="vote-good" title="Хороший комментарий!" v-on:click.prevent="voteComment(comment.id, 1);"></a>
-												<a v-if="comment.votes" href="#" class="vote-poor" title="Плохой комментарий!" v-on:click.prevent="voteComment(comment.id, -1);"></a>
-												<span :class="comment.voteClass">{{ comment.voteCount }}</span>
-											</span>
-										</span>
-										<Link class="comment-anchor" :href="`${route('item.name',[post.category.slug, post.id, post.slug])}#comment-${post.id}`" :id="`comment-${post.id}`">#{{ index }}</Link>
-										<span class="comment-author">{{ comment.name }}</span>
-										<span class="comment-date">{{ comment.date }}</span>
-										<div class="comment-body" :id="`comment-body-${comment.id}`" v-html="comment.text"></div>
-										<span class="comments-buttons">
-											<Link href="#" :onclick="`jcomments.quoteComment(${comment.id}); return false;`">Цитировать</Link>
-										</span>
-									</div>
-									<div class="clear"></div>
-								</div>
-								<div class="rbox_br">
-									<div class="rbox_bl">
-										<div class="rbox_b">&nbsp;</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</template>
-				</div>
-				<div id="comments-list-footer">
-					<a class="rss" :href="route('comment.rss',[post.category.slug, post.id, post.slug])" target="_blank">RSS лента комментариев этой записи</a>
-				</div>
-			</div>
-			<h4>Добавить комментарий</h4>
-			<Link id="addcomments" href="#addcomments"></Link>
-			<form id="comments-form" name="comments-form" action="javascript:void(null);">
-				<input type="hidden" name="_token" autocomplete="off" :value="$attrs.csrf_field" />
-				<div class="success-text" v-html="success" v-if="success"></div>
-				<div class="error-text" v-html="errors" v-if="errors"></div>
-				<GoogleCaptcha ref="googleCaptcha"></GoogleCaptcha>
-				<p>
-					<input v-model="name" id="comments-form-name" type="text" name="name" value="" maxlength="20" size="22" tabindex="1" class="">
-					<label for="comments-form-name">Имя (обязательное)</label>
-				</p>
-				<p>
-					<input v-model="email" id="comments-form-email" type="text" name="email" value="" size="22" tabindex="2">
-					<label for="comments-form-email">E-Mail (обязательное)</label>
-				</p>
-				<p><textarea v-model="message" id="comments-form-comment" name="message" cols="65" rows="8" tabindex="5"></textarea></p>
-				<div id="comments-form-buttons">
-					<div class="btn" id="comments-form-send"><div><a href="#" tabindex="7" v-on:click.prevent="saveComment();" title="Отправить (Ctrl+Enter)">Отправить</a></div></div>
-					<div class="btn" id="comments-form-cancel" style="display:none;"><div><Link href="#" tabindex="8" onclick="return false;" title="Отменить">Отменить</Link></div></div>
-					<div style="clear:both;"></div>
-				</div>
-				<input type="hidden" name="object_id" :value="post.id">
-				<input type="hidden" name="object_group" value="com_content">
-			</form>
-		</div>
+		<Comments :post="post"></Comments>
 	</template>
 </template>
 <script setup>
-import GoogleCaptcha from '@/Components/GoogleCaptcha.vue';
+	import Comments from '@/Components/Comments.vue';
 </script>
 <script>
-import MainLayout from '@/Layouts/MainLayout.vue';
-import {Link, router} from '@inertiajs/vue3';
+	import MainLayout from '@/Layouts/MainLayout.vue';
+	import {Link} from '@inertiajs/vue3';
 
-export default {
-	name: "Page",
-	components: {
-		Link
-	},
-	layout: MainLayout,
-	props: [
-		'post',
-	],
-	data() {
-			return {
-				errors: '',
-				success: '',
-				regexpEmail:/^(?:[a-z0-9._]+(?:[-_.]?[a-z0-9._]+)?@[a-z0-9_.-]+(?:\.?[a-z0-9]+)?\.[a-z]{2,5})$/i,
-				id: 0,
-				name:'',
-				email:'',
-				message:'',
-				post_id: 0,
-				recaptcha_response: '',
-				data: Object
-			};
-	},
-	methods: {
-		JCommentsInitializeForm(jcomments)
-			{
-				var jcEditor = new JCommentsEditor('comments-form-comment', true);
-					jcEditor.addButton('b','Жирный','Введите текст для форматирования');
-					jcEditor.addButton('i','Курсив','Введите текст для форматирования');
-					jcEditor.addButton('u','Подчеркнутый','Введите текст для форматирования');
-					jcEditor.addButton('s','Зачеркнутый','Введите текст для форматирования');
-					jcEditor.addButton('quote','Цитата','Введите текст цитаты');
-					jcEditor.addButton('custombbcode1','YouTube Video','Введите текст для форматирования','[youtube]','[/youtube]','bbcode-youtube','');
-					jcEditor.addButton('custombbcode4','Google Video','Введите текст для форматирования','[google]','[/google]','bbcode-google','');
-					jcEditor.addButton('custombbcode8','Facebook Video','Введите текст для форматирования','[fv]','[/fv]','bbcode-facebook','');
-					jcEditor.addButton('custombbcode10','Wikipedia','Введите текст для форматирования','[wiki]','[/wiki]','bbcode-wiki','');
-					jcEditor.initSmiles('/images/smilies');
-					jcEditor.addSmile(':D','laugh.gif');
-					jcEditor.addSmile(':lol:','lol.gif');
-					jcEditor.addSmile(':-)','smile.gif');
-					jcEditor.addSmile(';-)','wink.gif');
-					jcEditor.addSmile('8)','cool.gif');
-					jcEditor.addSmile(':-|','normal.gif');
-					jcEditor.addSmile(':-*','whistling.gif');
-					jcEditor.addSmile(':oops:','redface.gif');
-					jcEditor.addSmile(':sad:','sad.gif');
-					jcEditor.addSmile(':cry:','cry.gif');
-					jcEditor.addSmile(':o','surprised.gif');
-					jcEditor.addSmile(':-?','confused.gif');
-					jcEditor.addSmile(':-x','sick.gif');
-					jcEditor.addSmile(':eek:','shocked.gif');
-					jcEditor.addSmile(':zzz','sleeping.gif');
-					jcEditor.addSmile(':P','tongue.gif');
-					jcEditor.addSmile(':roll:','rolleyes.gif');
-					jcEditor.addSmile(':sigh:','unsure.gif');
-					jcEditor.addCounter(1000, 'Осталось:', ' символов', 'counter');
-					jcomments.setForm(new JCommentsForm('comments-form', jcEditor));
-			},
-			clearParams()
-			{
-				this.$data.errors = '';
-				this.$data.success = '';
-			},
-			voteComment(id, vote)
-			{
-				let data = {
-					vote : vote
-				};
-
-				axios.patch(route('post.update', {id}), data)
-				.then(res => {
-					router.reload();
-				})
-				.catch(res => {
-					this.errors = res.data;
-				});
-				return false;
-			},
-			saveComment()
-			{
-				this.clearParams();
-				this.checkName();
-				this.checkEMail();
-				this.checkMessage();
-
-				if (this.isError()) return false;
-				this.getData();
-				this.sendAjax();
-				return false;
-				
-			},
-			checkName()
-			{
-				if (this.name.length < 2) {
-					this.$data.errors += '<p>Имя не введено</p>';
-				}
-			},
-			checkEMail() 
-			{
-				if (!this.email.match(this.$data.regexpEmail)) {
-					this.$data.errors += '<p>Некорректно введен е-майл</p>';
-				}
-			},
-			checkMessage()
-			{
-				if (this.message.length < 2) {
-					this.$data.errors += '<p>Не введено сообщение</p>';
-				}
-			},
-			isError()
-			{
-				return this.$data.errors != '' ? true : false;
-			},
-			getData()
-			{
-				this.$data.name		= this.name;
-				this.$data.email	= this.email;
-				this.$data.message	= this.message;
-				this.$data._token	= this.csrf_field;
-				this.$data.post_id	= this.post.id;
-				this.recaptcha_response	= document.getElementById('recaptchaResponse').value;
-			},
-			sendAjax()
-			{
-				axios.post(route('comment.store', this.$data))
-				.then(res => {
-					let success = res.data.success;
-					router.reload();
-					if (success == 1)
-					{
-						this.success = '<p>Спасибо! Сообщение успешно добавлено!</p>';
-					}
-				})
-				.catch(res => {
-					this.$data.errors += '<p>' + res.response.data.message + '</p>';
-					this.$refs.googleCaptcha.initReCaptcha();
-				});
-				return false;
-			}
-	},
-	mounted()
-	{
-		var jcomments=new JComments( this.post.id , 'com_content',route('comment.ajax'));
-		jcomments.setList('comments-list');
-		this.JCommentsInitializeForm(jcomments);
+	export default {
+		name: "Page",
+		components: {
+			Link
+		},
+		layout: MainLayout,
+		props: [
+			'post',
+		]
 	}
-}
 </script>
